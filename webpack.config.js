458357db1b8864a766ac93876f505fb2dd35ff60
Artiom <sinigur2006@gmail.com>
2020-04-10 19:21:57 -0400

    Extract css styles into separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 // https://webpack.js.org/configuration/mode/
 // https://www.valentinog.com/blog/babel/
-
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 // Get absolute path console.log(__dirname)
 // path.join - concatenate public folder to absolute path
 
@@ -15,6 +16,12 @@ module.exports = (env) => {
             filename: 'bundle.js'
         },
         mode: 'development',
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+                ignoreOrder: false, // Enable to remove warnings about conflicting order
+            }),
+        ],
         module: {
             rules: [{
                 loader: 'babel-loader',
@@ -22,10 +29,32 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        } 
+                    }
+                ]
             }]
         },
-        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             compress: true,
