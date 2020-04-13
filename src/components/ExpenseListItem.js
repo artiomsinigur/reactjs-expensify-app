a60c4f69ed25091b2c0186138e3828e69c5127a0
Artiom <sinigur2006@gmail.com>
2020-04-13 16:50:45 -0400

    Format data and currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 export default function ExpenseListItem({ dispatch, id, desc, amount, createdAt }) {
+    // const formatAmount = new Intl.NumberFormat('us-US', {style: 'currency', currency: 'USD'}).format(amount / 100)
+    const numeralFormat = numeral(amount / 100).format('$0,0.00a')
     return (
         <tr>
             <th>
@@ -9,8 +13,8 @@ export default function ExpenseListItem({ dispatch, id, desc, amount, createdAt 
                     {desc}
                 </Link>
             </th>
-            <td>{amount}</td>
-            <td>{createdAt}</td>
+            <td>{numeralFormat}</td>
+            <td>{moment(createdAt).format('MMM Do YYYY')}</td>
             {/* <td><button type="button" onClick={() => {
                 dispatch(removeExpenseById(id))
             }}>Remove</button></td> */}
